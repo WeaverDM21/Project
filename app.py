6e36d055cd6d503869863419b560db199c7fe2d4
WEAVERDM21 <weaverdm21@gcc.edu>
2024-11-06 12:51:12 -0500

    Working on app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return render_template('index.html')
+    makeRequest()
+    return render_template('home_page.html')
+
+def makeRequest():
+    api_key = 'fafe5690'
+    base_url = 'http://www.omdbapi.com/'
+    title = "Inception"
+    params = {
+        'apikey': api_key,
+        't': title
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)  # This will print the movie data
+    else:
+        print(f"Error: {response.status_code}")
